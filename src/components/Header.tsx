91cbca738249b9ba476039b3202c101475f2a030
Sergii Bondarenko <sergibondarenko@gmail.com>
2021-10-05 18:45:29 +0200

    Add stuff to the heades: account, orders and basket.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { AmazonLogoImage } from '../components/images';
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'; 
 
-function TopSearchInput() {
+function AmazonLogo() {
+  return (
+    <div className="mt-2 flex flex-grow items-center sm:flex-grow-0">
+      <AmazonLogoImage width={150} height={40} />
+    </div>
+  );
+}
+
+function SearchInput() {
   return (
     <div
       className="hidden sm:flex items-center bg-yellow-400 hover:bg-yellow-500 h-10 rounded-md flex-grow cursor-pointer"
@@ -13,13 +21,46 @@ function TopSearchInput() {
   );
 }
 
-function TopNav() {
+function Account() {
   return (
-    <div className="flex flex-grow items-center bg-amazon_blue p-1 py-2">
-      <div className="mt-2 flex flex-grow items-center sm:flex-grow-0">
-        <AmazonLogoImage width={150} height={40} />
-      </div>
-      <TopSearchInput />
+    <div className="link">
+      <p>Hello, John Baker!</p>
+      <p className="font-extrabold md:text-sm">Account & Lists</p>
+    </div>
+  );
+}
+
+function Orders() {
+  return (
+    <div className="link">
+      <p>Returns</p>
+      <p className="font-extrabold md:text-sm">& Orders</p>
+    </div>
+  );
+}
+
+function ShoppingCartItemsCounter() {
+  return (
+    <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+  );
+}
+
+function ShoppingCart() {
+  return (
+    <div className="link relative flex items-center">
+      <ShoppingCartItemsCounter />
+      <ShoppingCartIcon className="h-10" />
+      <p className="hidden font-extrabold md:text-sm md:inline mt-2">Basket</p>
+    </div>
+  );
+}
+
+function TopNavRight() {
+  return (
+    <div className="text-white flex items-center space-x-6 text-xs mx-6 whitespace-nowrap">
+      <Account />
+      <Orders />
+      <ShoppingCart />
     </div>
   );
 }
@@ -27,7 +68,11 @@ function TopNav() {
 export function Header() {
   return (
     <header>
-      <TopNav />
+      <div className="flex flex-grow items-center bg-amazon_blue p-1 py-2">
+        <AmazonLogo />
+        <SearchInput />
+        <TopNavRight />
+      </div>
     </header>
   );
 }
