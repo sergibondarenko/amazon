c71d262df69effb6c3e981df2fa93db0483cf199
Sergii Bondarenko <sergibondarenko@gmail.com>
2021-10-05 19:00:10 +0200

    Implement the bottom nav in the header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -65,6 +65,29 @@ function TopNavRight() {
   );
 }
 
+function BottomNavMenu() {
+  return (
+    <p className="link flex items-center"><MenuIcon className="h-6 mr-1" /> All</p>
+  );
+}
+
+function BottomNav() {
+  return (
+    <div className="flex items-center bg-amazon_blue-light text-sm text-white space-x-3">
+      <BottomNavMenu />
+      <p className="link">Prime Video</p>
+      <p className="link">Amazon Basics</p>
+      <p className="link">Today's Deals</p>
+      <p className="link hidden lg:inline-flex">Electronics</p>
+      <p className="link hidden lg:inline-flex">Food & Grocery</p>
+      <p className="link hidden lg:inline-flex">Prime</p>
+      <p className="link hidden lg:inline-flex">Buy Again</p>
+      <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+      <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+    </div>
+  );
+}
+
 export function Header() {
   return (
     <header>
@@ -73,6 +96,7 @@ export function Header() {
         <SearchInput />
         <TopNavRight />
       </div>
+      <BottomNav />
     </header>
   );
 }
