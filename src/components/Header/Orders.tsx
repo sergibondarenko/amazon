e5a2935196581215dce205cae7a84dcb5640d494
Sergii Bondarenko <sergibondarenko@gmail.com>
2021-10-10 21:20:45 +0200

    Push user to the orders page after the orders link in the header clicked.

--- src/components/Header/Orders.tsx
+++ src/components/Header/Orders.tsx
@@ -1,8 +1,15 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export function Orders() {
+  const router = useRouter();
+
+  function handleClick() {
+    router.push('/orders');
+  }
+
   return (
-    <div className="link">
+    <div className="link" onClick={handleClick}>
       <p>Returns</p>
       <p className="font-extrabold md:text-sm">& Orders</p>
     </div>
